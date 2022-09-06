d576ab2b6c5d4b5e7ba8b76c6159988b1a5b06d0
YANN BERTHELOT <yann.berthelot@capgemini.com>
2022-09-06 12:07:19 +0200

    refacto daily variation with partial

--- src/easygrid/config/pymgrid_config.py
+++ src/easygrid/config/pymgrid_config.py
@@ -3,15 +3,9 @@
 """
 import numpy as np
 
+from easygrid.config.realistic_config import grid_config, mg_config
 from easygrid.data.data_utils import DATA_FOLDER, get_indexes
-from easygrid.math_utils import get_hourly_variation
-from easygrid.types import (
-    BatteryConfig,
-    GridConfig,
-    LoadConfig,
-    MicrogridConfig,
-    PvConfig,
-)
+from easygrid.types import BatteryConfig, LoadConfig, PvConfig
 
 INDEXES = get_indexes(DATA_FOLDER)
 LOAD = INDEXES["load"][3]  # RefBldgPrimarySchoolNew2004_v1.3_7.1_2A_USA_TX_HOUSTON.csv
@@ -30,20 +24,6 @@ battery_config: BatteryConfig = {
     "overcharge_penalty": 1,
 }
 
-grid_config: GridConfig = {
-    "import_prices": list(
-        i
-        for i in get_hourly_variation(
-            max_val=10, min_val=6, period=24, time_max=15, time_min=3, size=MAX_TIMESTEP
-        )
-    ),
-    "export_prices": list(
-        i
-        for i in get_hourly_variation(
-            max_val=5, min_val=1, period=24, time_max=15, time_min=3, size=MAX_TIMESTEP
-        )
-    ),
-}
 pv_config: PvConfig = {
     "pv_production_ts": PV,
 }
@@ -51,11 +31,7 @@ pv_config: PvConfig = {
 load_config: LoadConfig = {
     "load_ts": LOAD,
 }
-mg_config: MicrogridConfig = {
-    "max_timestep": MAX_TIMESTEP,
-    "overprod_penalty": 1,
-    "underprod_penalty": 1,
-}
+
 config = {
     "BATTERY": battery_config,
     "MICROGRID": mg_config,
